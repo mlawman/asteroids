86bddf8ead84d586ff386687c4282f00b44c7a9e
mlawman <michaeljlawman@hotmail.com>
2024-10-01 12:57:42 -0400

    complete game

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
-def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+def main():           
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Shot.containers = (shots, updatable, drawable)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                return        
+                    
+        for update in updatable:
+            update.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_for_collision(player):
+                print("Game over!")
                 return
-        screen.fill("#000000")
-        pygame.display.flip()
+            for shot in shots:
+                if asteroid.check_for_collision(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        screen.fill("black")
+
+        for draw in drawable:            
+            draw.draw(screen)
+        
+        pygame.display.flip()        
+        
+        #limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
